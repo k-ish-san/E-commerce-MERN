d8571e076fad44c6f90b6858948524bc03f4d8a7
Samrudh Kishsan P M <samrudhkishsanpm@gmail.com>
2025-02-08 20:03:47 +0530

    commit userRoutes

--- E-commerce-MERN/backend/routes/userRoutes.js
+++ E-commerce-MERN/backend/routes/userRoutes.js
@@ -4,3 +4,20 @@ const jwt = require("jsonwebtoken");
 
 
 const router = express.Router();
+
+// @route POST api/users/register
+// @desc Register user
+// @access Public
+router.post("/register", async (req, res) => {
+    const { name, email, password } = req.body;
+
+    try {
+        // Registration logic
+        res.send({ name, email, password })
+    } catch (error) {
+        console.error(error);
+        res.status(500).send( "Server error" );
+    }
+});
+
+module.exports = router;
