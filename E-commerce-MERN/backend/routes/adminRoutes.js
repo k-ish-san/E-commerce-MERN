644e3aed1e906ef6fb2d6d4dfa0f516302b19436
Samrudh Kishsan P M <samrudhkishsanpm@gmail.com>
2025-05-21 16:41:38 +0530

    Implement user update and delete functionality, and add product and order management routes for admin

--- E-commerce-MERN/backend/routes/adminRoutes.js
+++ E-commerce-MERN/backend/routes/adminRoutes.js
@@ -47,7 +47,44 @@ router.post('/', protect, admin, async (req, res) => {
 // @desc    Update a user information
 // @access  Private/Admin
 router.put('/:id', protect, admin, async (req, res) => {
-    const { name, email, password, role } = req.body;
-
+    try {
+        const user = await User.findById(req.params.id);
+        if (user) {
+         
+            // Update user information
+            user.name = req.body.name || user.name;
+            user.email = req.body.email || user.email;
+            user.role = req.body.role || user.role;
+        }
+        const updatedUser = await user.save();
+        res.json({ message: 'User updated successfully', user: updatedUser });
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error' });
+    }
 });
+
+
+// @route   DELETE /api/admin/users/:id
+// @desc    Delete a user
+// @access  Private/Admin
+router.delete('/:id', protect, admin, async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (user) {
+         
+            await user.deleteOne();
+            res.json({ message: 'User deleted successfully' });
+        }
+        else {
+            res.status(404).json({ message: 'User not found' });
+        }   
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+
+
 module.exports = router;
