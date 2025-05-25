0093495bc8150b4f7311f7d5292008c815bade37
Samrudh Kishsan P M <samrudhkishsanpm@gmail.com>
2025-05-25 15:28:40 +0530

    fix: Remove temporary log of MONGO_URI from database connection

--- E-commerce-MERN/backend/config/db.js
+++ E-commerce-MERN/backend/config/db.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-      console.log("MONGO_URI:", process.env.MONGO_URI); // TEMP log
       await mongoose.connect(process.env.MONGO_URI);
       console.log("MongoDB Connected: Successfully");
     }
