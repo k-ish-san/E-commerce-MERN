8c4788ebff843748f0943ee0d55e81c550b13ba6
Samrudh Kishsan P M <samrudhkishsanpm@gmail.com>
2025-05-28 15:31:02 +0530

    fix: Remove server listen log statement from server.js

--- E-commerce-MERN/backend/server.js
+++ E-commerce-MERN/backend/server.js
@@ -63,6 +63,4 @@ app.use("/api/admin/users", adminRoutes);
 app.use("/api/admin/products", productAdminRoutes);
 app.use("/api/admin/orders", adminOrderRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost:${PORT}`);
-});
+
