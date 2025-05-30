beaf13ae2409bc12359b3890bad391c1adda68c0
Samrudh Kishsan P M <samrudhkishsanpm@gmail.com>
2025-05-30 15:28:33 +0530

    fix: Move PORT declaration and server listen log for better organization

--- E-commerce-MERN/backend/server.js
+++ E-commerce-MERN/backend/server.js
@@ -19,6 +19,7 @@ const app = express();
 
 dotenv.config();
 
+const PORT = process.env.PORT || 3000;
 
 // connect to mongodb
 connectDB();
@@ -65,3 +66,7 @@ app.use("/api", subscribeRoute);
 app.use("/api/admin/users", adminRoutes);
 app.use("/api/admin/products", productAdminRoutes);
 app.use("/api/admin/orders", adminOrderRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
