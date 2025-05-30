ba54bc942932a5c4bac502ba076d8614032498da
Samrudh Kishsan P M <samrudhkishsanpm@gmail.com>
2025-05-30 15:39:48 +0530

    refactor: Remove server listen logic to export app for improved modularity

--- E-commerce-MERN/backend/server.js
+++ E-commerce-MERN/backend/server.js
@@ -67,6 +67,5 @@ app.use("/api/admin/users", adminRoutes);
 app.use("/api/admin/products", productAdminRoutes);
 app.use("/api/admin/orders", adminOrderRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost:${PORT}`);
-});
+module.exports = app;
+
