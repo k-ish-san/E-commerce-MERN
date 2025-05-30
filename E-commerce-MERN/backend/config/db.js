624628dca791a63ba5acaa5b61f67b04d278eaa5
Samrudh Kishsan P M <samrudhkishsanpm@gmail.com>
2025-05-30 16:52:55 +0530

    fix: Add options for mongoose connection to improve compatibility

--- E-commerce-MERN/backend/config/db.js
+++ E-commerce-MERN/backend/config/db.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-      await mongoose.connect(process.env.MONGO_URI);
+      await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
       console.log("MongoDB Connected: Successfully");
     }
     catch (err) {
@@ -23,4 +26,3 @@ module.exports = connectDB;
 //   "builds": [{ "src": "server.js", "use": "@vercel/node" }],
 //   "routes": [{ "src": "/*", "dest": "server.js" }]
 // }
-
