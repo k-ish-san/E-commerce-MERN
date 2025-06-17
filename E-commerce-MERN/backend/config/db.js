abf7cd05f2bd8ab0a5cf153c261fa2350a1a22e5
Samrudh Kishsan P M <126341963+k-ish-san@users.noreply.github.com>
2025-06-17 12:27:35 +0530

    db update

--- E-commerce-MERN/backend/config/db.js
+++ E-commerce-MERN/backend/config/db.js
@@ -1,20 +1,16 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    try {
-      await mongoose.connect(process.env.MONGO_URI);
-      console.log("MongoDB Connected: Successfully");
-    }
-    catch (err) {
-        console.error("Mongodb connection failed.", err.message);
-        process.exit(1);
-        
-    }
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`✅ MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`❌ MongoDB connection error: ${error.message}`);
+    process.exit(1);
+  }
 };
 
-module.exports = connectDB; 
-
-
+module.exports = connectDB;
 
 
 // {
